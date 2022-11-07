d7493153987334c71ebe6f71a0e881a4d284d8c2
Alba <albarula@gmail.com>
2022-11-07 17:59:30 +0100

    estructura

---
+++ src/components/CompFooter.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer id="footeradele">
+        <h3>FOLLOW ADELE</h3>
+        <div id="icons">
+          <a href="https://www.adele.com/" target="_blank">
+            <img id="iconfacebook" alt="facebook" src="../assets/Facebook.png">
+          </a>  
+            
+            <img id="icontwitter" alt="twitter" src="../assets/twitter.png">
+            <img id="iconyoutube" alt="facebook" src="../assets/youtube.png">
+            <img id="iconinstagram" alt="facebook" src="../assets/instagram.png">
+            <img id="iconspotify" alt="facebook" src="../assets/spotify.png">
+        </div>
+
+        <h4>@ 2022 SONY MUSIC ENTERTAINMENT</h4>
+
+        <div>
+            <h4>TERMS AND CONDITIONS</h4>
+            <h4>PRIVACY POLICY</h4>
+            <h4>HOW WE USE YOUR DATA</h4>
+        </div>
+
+    </footer>
+    
+</template>
+
+<script>
+
+export default {
+    name: 'CompFooter'
+}
+
+
+</script>
