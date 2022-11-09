d162f943e97cf2ae5a365ad07e39015f8188c7e5
Festival3224 <QSR^V9&h>
2022-11-09 18:12:50 +0100

    row API Conciertos

---
+++ src/components/AttractionModalView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="bg-slate-400 bg-opacity-50  w-full h-full top-0 fixed">
+        <div class="bg-white w-96 p-5 m-96 border-slate-50 rounded "
+             @click="closeModal">
+            <h1>{{ headerName }}</h1>
+            <img class="w-full" src="'imageUrl'" alt=""> <br>
+            <p class="text-xl text-gray-900">Attraction content</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+       props: ['headerName','imageUrl'],
+       methods: {
+        closeModal() {
+           this.$emit('close') 
+        }
+       } 
+    }
+</script>
+
--- src/components/ConciertosView.vue
+++ src/components/ConciertosView.vue
@@ -2,7 +2,7 @@
     <div class="grid grid-cols-3">
         <!-- Photo -->
         <div class="col-span-1">
-            <img src="../assets/adeleconc.png" alt="">
+            <img class="w-full" src="../assets/adeleconc.png" alt="">
         </div>
         <!-- Conciertos -->
         <div class="col-span-2 text-center bg-slate-50">
@@ -14,8 +14,11 @@
             <!-- Lista de Conciertos -->
             <div v-if="eventos">
                 <h3 class="text-gray-800 text-lg ">encontramos <span class="text-yellow-600 font-semibold  ">{{ eventos.page.totalElements }} </span> eventos:</h3>
-                <ul>
-                    <li v-for:="events in eventos._embedded.events" :key="events" class="my-2">
+                
+                <ul class="">
+                    <li v-for:="events in eventos._embedded.events" :key="events"
+                         class="md:w-1/2 lg:w-1/3 ml-2 mr-2 md:ml-auto md:mr-auto text-left shadow-sm md:shadow-md my-2 hover:bg-slate-200"
+                         @click="showAttraction">
                         {{events.name}} <br>
                         {{events.dates.start.localDate}} <br>
                         {{events._embedded.venues[0].name}} in 
@@ -23,21 +26,30 @@
                         {{events._embedded.venues[0].country.countryCode}}
                     </li>
                 </ul>
+
+                <div v-if="showAttractionModal">
+                    <AttractionModal :header-name = "eventos._embedded.events[0].name" 
+                                 :imageUrl = "eventos._embedded.events[0].images[0].url"
+                                 @close="showAttraction" />
+                </div>
             </div>
         </div>        
     </div>
+    
 </template>
 
 <script>
 //import { response } from 'express';
-
+import AttractionModal from './AttractionModalView.vue'
 export default {
     name: "ConciertosView",
+    components: { AttractionModal },
     data() {
         return {
             eventos: null,
             error:'',
-            key: 'o5gbvSYDrZAEYhCXE11sLyK8G0t5e4Qs'
+            key: 'o5gbvSYDrZAEYhCXE11sLyK8G0t5e4Qs',
+            showAttractionModal: false
         }
     },
     methods: {
@@ -48,6 +60,9 @@ export default {
                 .then(datos => this.eventos = datos)
                 //.then(datos => console.log(datos))
                 .catch((e) => (this.error =e));
+        },
+        showAttraction() {
+            this.showAttractionModal = !this.showAttractionModal
         }
     }
 }
