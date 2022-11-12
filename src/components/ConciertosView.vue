eb961e55b44f7dcec44c68032df0b0698cc5c7a1
Festival3224 <QSR^V9&h>
2022-11-12 02:31:37 +0100

    complete pagination

--- src/components/ConciertosView.vue
+++ src/components/ConciertosView.vue
@@ -9,15 +9,16 @@
             <h1 class="text-4xl font-serif uppercase py-10">Conciertos</h1>
             <button type="submit"
                     class="h-10 w-1/4 border-2 border-gray-600 rounded-lg"
-                    v-on:click="getShows()">find
+                    v-on:click="getShows(0)">find
             </button>
             <!-- Lista de Conciertos -->
             <div v-if="eventos">
-                <h3 class="text-gray-800 text-lg ">encontramos <span class="text-yellow-600 font-semibold  ">{{ eventos.page.totalElements }} </span> eventos, <span>(in total {{ totalPages() }} pages )</span> </h3>
-                
-                <ul class="">
-                    <!-- <li v-for:="events in eventos._embedded.events" :key="events" -->
-                        <li v-for:="events in dataPerPage" :key="events"
+                <h3 class="text-gray-800 text-lg ">encontramos <span class="text-yellow-600 font-semibold  ">{{ eventos.page.totalElements }}
+                 </span> eventos, <span>({{ totalPages() }} paginas x {{ elementsPerPage }} )</span>
+                 estamos a {{ currentPage+1 }}
+                </h3>              
+                <ul class="">                                          
+                    <li v-for:="events in eventos._embedded.events" :key="events"        
                          class="md:w-1/2 lg:w-1/3 ml-2 mr-2 md:ml-auto md:mr-auto text-left shadow-sm md:shadow-md my-2 hover:bg-slate-200"
                          @click="showAttraction">
                         {{events.name}} <br>
@@ -27,11 +28,20 @@
                         {{events._embedded.venues[0].country.countryCode}}
                     </li>
                 </ul>
-                <nav class="panel-footer bg-purple-300 w-96 h-10 ml-auto mr-auto">
-                    <ul class="m-8 items-center flex justify-between">
-                        <li id="prev" class="previous"><a href="#">&larr;</a></li>
-                        <li v-for:="page in totalPages()" v-on:click="getDataPage(page)" class="pageItem"><a href="#">{{ page }}</a></li>
-                        <li id="next" class="next"><a href="#">&rarr;</a></li>
+                <nav class="bg-slate-200 md:w-1/2 lg:w-1/3 h-12 ml-auto mr-auto">
+                    <ul class="h-12 m-2 flex flex-row items-center justify-between">
+                        <li v-on:click.prevent="prevClick()">
+                            <a class="py-2 px-3 ml-0 bg-white hover:bg-slate-300 border rounded-l-md border-gray-300" href="#">
+                                <i class="w-5 h-5 fa-sharp fa-solid fa-arrow-left text-lg text-slate-500"></i>
+                                <!-- &larr; -->
+                            </a>
+                        </li>                        
+                        <li v-on:click.prevent="nextClick()">
+                            <a class="py-2 px-3 mr-0 bg-white hover:bg-slate-300 border rounded-r-md border-gray-300" href="#">
+                                <i class="w-5 h-5 fa-sharp fa-solid fa-arrow-right text-lg text-slate-500"></i>
+                                <!-- &rarr; -->
+                            </a>
+                        </li>
                     </ul>
                 </nav>
 
@@ -47,7 +57,6 @@
 </template>
 
 <script>
-//import { response } from 'express';
 import AttractionModal from './AttractionModalView.vue'
 export default {
     name: "ConciertosView",
@@ -58,43 +67,42 @@ export default {
             error:'',
             key: 'o5gbvSYDrZAEYhCXE11sLyK8G0t5e4Qs',
             showAttractionModal: false,
-            elementsPerPage: 6,
-            dataPerPage: []
+            elementsPerPage: 8,
+            currentPage:0
         }
     },
-    // mounted(){
-    //     //this.getShows();
-    //     this.getDataPage(1);
-    // },
     methods: {
-        getShows() {
-            let url = `https://app.ticketmaster.com/discovery/v2/events.json?attractionId=K8vZ917Gku7&size=200&sort=date,asc&apikey=${this.key}`;
+        getShows(numPage) {
+            this.currentPage = numPage;
+          
+            let url = `https://app.ticketmaster.com/discovery/v2/events.json?attractionId=K8vZ917Gku7&sort=date,asc&apikey=${this.key}&size=${this.elementsPerPage}&page=${this.currentPage}`;
             fetch(url)
                 .then(response => response.json())
-                .then(datos => this.eventos = datos)
+                .then(datos => this.eventos = datos)                
                 //.then(datos => console.log(datos))
                 .catch((e) => (this.error =e));
-            console.log(this.getDataPage(1));
+        },
+        nextClick() {
+            this.currentPage++;
+            if (this.currentPage > (this.totalPages()-1)){
+                this.currentPage = 0
+            } 
+            this.getShows(this.currentPage)
+        },
+        prevClick() {
+            this.currentPage--;
+            if (this.currentPage < 0) {
+                this.currentPage = this.totalPages()-1
+            } 
+            this.getShows(this.currentPage)
         },
         showAttraction() {
             this.showAttractionModal = !this.showAttractionModal
         },
         totalPages() {
             return Math.ceil(this.eventos.page.totalElements / this.elementsPerPage)
-        },
-        getDataPage(numPage) {
-            this.dataPerPage = [];
-            let ini = (numPage*this.elementsPerPage) - this.elementsPerPage;
-            //let fin = 0;
-            let fin = numPage*this.elementsPerPage;
-            let tail = this.eventos.page.totalElements % this.elementsPerPage;
-            if ((tail != 0) && (numPage === this.totalPages()))
-             {
-                fin = fin-this.elementsPerPage + tail
-             }
-           
-             this.dataPerPage = this.eventos._embedded.events.slice(ini,fin);
         }
+        
     }
 }
 
