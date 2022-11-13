07ed16b6fef292ce4a8e1f786389b99e89fa9c44
Festival3224 <QSR^V9&h>
2022-11-13 17:38:22 +0100

    complete sector Conciertos

--- src/components/AttractionModalView.vue
+++ src/components/AttractionModalView.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="bg-slate-400 bg-opacity-50  w-full h-full top-0 fixed">
-        <div class="bg-white w-96 p-5 m-96 border-slate-50 rounded "
+    <div class="bg-slate-400 bg-opacity-50 w-full h-full top-0 sm:left-1/3 fixed">
+        <div class="bg-white  max-w-xs lg:max-w-lg
+             mt-28 md:mt-48 lg:mt-80
+            ml-6 sm:ml-10 md:ml-40  xl:ml-80 p-5 border-slate-50 rounded"
              @click="closeModal">
-            <h1>{{ headerName }}</h1>
+            <!-- <h1>{{ headerName }}</h1>
             <img class="w-full" src="'imageUrl'" alt=""> <br>
-            <p class="text-xl text-gray-900">Attraction content</p>
+            <p class="text-xl text-gray-900">Attraction content</p> -->
+            <p class="">Aquí tiene que haber más información del evento. Por ahora no está echo. Estoy trabajando en ello... <br></p>
+            <p class="mt-6">Para cerrar esta ventana, hacer un click en ella.</p>
         </div>
     </div>
 </template>
--- src/components/ConciertosView.vue
+++ src/components/ConciertosView.vue
@@ -1,27 +1,27 @@
 <template>
-    <div id="concertscomp" class="grid md:grid-cols-2 lg:grid-cols-3">
+    <div id="concertscomp" class="grid sm:grid-cols-3">
         <!-- Photo -->
-        <div class="md:grid md:grid-cols-2 md:col-span-1 lg:grid-cols-3 lg:col-span-1 md:bg-yellow-200 lg:bg-gray-900">
+        <div class="row-span-1 sm:grid-cols-3 sm:col-span-1 bg-stone-900  ">
             <img class="w-full md:col-span-3" src="../assets/adeleconc.png" alt="">
         </div>
         <!-- Conciertos -->
-        <div class="md:grid-cols-2 md:col-span-1 lg:grid-cols-3 lg:col-span-2 text-center
-                    md:bg-yellow-500 lg:bg-white">
+        <div class="row-span-1 sm:grid-cols-3 sm:col-span-2 text-center bg-white">                  
             <h1 class="text-2xl md:text-3xl lg:text-4xl font-serif uppercase py-3 md:py-5 xl:py-10">Conciertos</h1>
             <button type="submit"
                     class="h-10 w-1/4 text-gray-900 bg-white border border-gray-300
                              focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 2xl:my-10 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                    v-on:click="getShows(0)">find
+                    v-on:click="getShows(0)">buscar
             </button>
+
             <!-- Lista de Conciertos -->
             <div v-if="eventos"
-                 class="w-10/12 lg:w-2/3 xl:w-1/2 ml-auto mr-auto">
+                 class="w-10/12 sm:w-11/12 md:w-9/12 lg:w-2/3 xl:w-1/2 ml-auto mr-auto">
                 <!-- <h3 class="text-gray-800 text-lg ">encontramos <span class="text-yellow-600 font-semibold  ">{{ eventos.page.totalElements }}</span> eventos, <span>({{ totalPages() }} paginas x {{ elementsPerPage }} )</span>estamos a {{ currentPage+1 }}</h3>    -->
                 <ul>                                          
                     <li v-for:="events in eventos._embedded.events" :key="events"        
                          class="text-left shadow-sm md:shadow-md my-2 hover:bg-slate-200"
                          @click="showAttraction">
-                        {{events.name}} <br>
+                        <h3 class=""> {{events.name}} <br></h3>
                         {{events.dates.start.localDate}} <br>
                         {{events._embedded.venues[0].name}} in 
                         {{events._embedded.venues[0].city.name}},
@@ -29,31 +29,32 @@
                     </li>
                 </ul>
 
-                <nav class="bg-slate-200 h-12 ml-auto mr-auto rounded-md">
-                    <ul class="h-12 m-2 flex flex-row items-center justify-between">
+                <nav class="bg-slate-200 h-8 sm:h-10 lg:h-12 ml-auto mr-auto rounded-md">
+                    <ul class="h-8 sm:h-10 lg:h-12 m-2 flex flex-row items-center justify-between">
                         <li v-on:click.prevent="prevClick()">
-                            <a class="py-2 px-3 ml-0 bg-white hover:bg-slate-300 border rounded-l-md border-gray-300" href="#">
-                                <i class="w-5 h-5 fa-sharp fa-solid fa-arrow-left text-lg text-slate-500"></i>
+                            <a class="py-1 sm:py-2 px-3 ml-0 bg-white hover:bg-slate-300 border rounded-l-md border-gray-300" href="#">
+                                <i class="w-3 h-3 sm:w-4 sm:h-4 lg:w-5 lg:h-5 fa-sharp fa-solid fa-arrow-left text-sm lg:text-lg text-slate-500"></i>
                                 <!-- &larr; -->
                             </a>
                         </li>                        
                         <li v-on:click.prevent="nextClick()">
-                            <a class="py-2 px-3 mr-0 bg-white hover:bg-slate-300 border rounded-r-md border-gray-300" href="#">
-                                <i class="w-5 h-5 fa-sharp fa-solid fa-arrow-right text-lg text-slate-500"></i>
+                            <a class="py-1 sm:py-2 px-3 mr-0 bg-white hover:bg-slate-300 border rounded-r-md border-gray-300" href="#">
+                                <i class="w-3 h-3 lg:w-5 lg:h-5 fa-sharp fa-solid fa-arrow-right text-sm lg:text-lg text-slate-500"></i>
                                 <!-- &rarr; -->
                             </a>
                         </li>
                     </ul>
                 </nav>
 
-                <div v-if="showAttractionModal">
-                    <AttractionModal :header-name = "eventos._embedded.events[0].name" 
-                                 :imageUrl = "eventos._embedded.events[0].images[0].url"
-                                 @close="showAttraction" />
-                </div>
             </div>
-        </div>        
+        </div>      
     </div>
+
+    <div v-if="showAttractionModal">
+        <AttractionModal :header-name = "eventos._embedded.events[0].name" 
+                         :imageUrl = "eventos._embedded.events[0].images[0].url"
+                         @close="showAttraction" />
+    </div>  
     
 </template>
 
