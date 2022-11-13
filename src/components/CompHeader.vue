bda56b147d2b1ddf4f842bfaea8a7db5191c1248
Alba <albarula@gmail.com>
2022-11-13 20:06:36 +0100

    lo que he podido de responsive

--- src/components/CompHeader.vue
+++ src/components/CompHeader.vue
@@ -7,7 +7,7 @@
                 <img src="../assets/1ADELE.png" class="mr-3 h-6 sm:h-10 md:h-14 lg:h-24" alt="Adele Logo" />
             </a>
             <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300" aria-controls="navbar-solid-bg" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
+           
             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </button>
             <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
